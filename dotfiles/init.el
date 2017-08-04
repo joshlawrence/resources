40c28105925acda373629d5cd2255bc4a0e4cf08
Josh Lawrence <josh@joshlawrence.me>
2017-08-04 15:57:50 -0500

    added alpine doc

--- dotfiles/init.el
+++ dotfiles/init.el
@@ -1,4 +1,5 @@
 ;; .emacs init file
+;; August 1, 2017 at 11:25:20 AM CDT
 
 ;; setup package repos
 (require 'package)
@@ -7,27 +8,27 @@
 (package-initialize)
 
 ;; full-screen
-(setq ns-use-native-fullscreen nil)
+;(setq ns-use-native-fullscreen nil)
 
 ;; disable auto-save
 (setq auto-save-default nil)
 (setq make-backup-files nil)
 
-;; 80 column rule
+;; wrap at 80 columns
+(add-hook 'text-mode-hook 'turn-on-auto-fill)
 (setq-default fill-column 80)
 
 ;; neotree config
-(require 'neotree)
-(global-set-key [f8] 'neotree-toggle)
-(setq neo-smart-open t)
-(setq neo-theme 'ascii)
-(setq neo-window-fixed-size nil)
+;(require 'neotree)
+;(global-set-key [f8] 'neotree-toggle)
+;(setq neo-smart-open t)
+;(setq neo-theme 'ascii)
+;(setq neo-window-fixed-size nil)
 
 ;; themes
 ;;(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
-(load-theme 'zenburn t)
+;(load-theme 'base16-ashes t)
 
 ;; move customize blocks to another file
 (setq custom-file "~/.emacs.d/custom.el")
 (load custom-file)
-
--- links.md
+++ links.md
@@ -3,4 +3,6 @@
 - http://git.2f30.org/
 - http://git.r-36.net/
 - http://git.suckless.org/
-
+- https://lyngvaer.no/ (good css)
+- https://major.io/ (more good css)
+- https://nixers.net/ (forum)
