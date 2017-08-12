11e64142112b44ed27ee943296f655d075bdaadb
Josh Lawrence <josh@joshlawrence.me>
2017-08-12 13:16:05 -0500

    added git setup

---
+++ scripts/setup-git.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+#
+# I'm tired of typing this...
+
+git config --global user.name "Josh Lawrence"
+git config --global user.email "josh.lawrence@dmpinvestments.net"
+git config --global color.ui true
+git config --global push.default simple
+
+# editor
+git config --global core.editor vim
+
+# linux?
+#git config --global credential.helper cache
+#git config --global credential.helper 'cache --timeout=3600'
+
+# osx?
+#git config --global credential.helper osxkeychain
