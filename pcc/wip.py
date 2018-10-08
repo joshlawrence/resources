9bd949e71d33491ea5df745b5600a4d13912fe90
Josh Lawrence <josh.lawrence@dmpinvestments.net>
2018-10-08 16:40:22 -0500

    MORE PYTHON WORK

--- pcc/wip.py
+++ pcc/wip.py
@@ -1,8 +1,18 @@
 #!/usr/bin/env python3
 
+# print only lines with jasmin.gonzalez.rpsg is in the AGENT column
 
-prompt = "Now we will ask you your age!"
-prompt += "\nHow old are you? "
+import csv
 
-age = input(prompt)
-print("You are " + str(age) + " years old.")
+#data_file =  (open('data.csv', 'w'))
+#csv_writer = csv.writer(data_file, delimiter = ',')
+line_count = 0
+
+with open('log.csv', 'r') as csv_file:
+    csv_reader = csv.DictReader(csv_file)
+    for row in csv_reader:
+        if row['AGENT'] == 'jasmin.gonzalez.rpsg':
+            print(row)
+            line_count += 1
+
+    print(f'Processed {line_count} line(s).')
