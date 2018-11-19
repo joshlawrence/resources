52edb4627b222aef5d424102a3ee7d992347909d
Josh Lawrence <josh@joshlawrence.me>
2018-11-19 13:39:58 -0600

    cleanup, plus added resources.txt

--- scripts/dumpPageLinks.sh
+++ scripts/dumpPageLinks.sh
@@ -2,8 +2,14 @@
 #
 # dumps all links on a single page
 
+# check for lynx
+command -v lynx >/dev/null 2>&1 || { echo "This script requires lynx." >&2; }
+
 printf "%s\n" "Enter the url and press [ENTER] "
 read -ep "url: " url
 
+printf "%s\n" "Enter the textfile name and press [ENTER] "
+read -ep "filename: " f_name
+
 lynxopts=( -dump -listonly -nonumbers -notitle )
-lynx "${lynxopts[@]}" "$url" > linklist.txt
+lynx "${lynxopts[@]}" "$url" > "$f_name.txt"
