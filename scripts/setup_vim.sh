665170d6a6c7ddfe7bd95c6caae28f593b467018
Josh Lawrence <josh.lawrence@dmpinvestments.net>
2019-03-05 09:13:29 -0600

    updated vim setup script and vimrc file

--- scripts/setup_vim.sh
+++ scripts/setup_vim.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-mkdir -p $HOME/.vim/autoload $HOME/.vim/bundle
+mkdir $HOME/.vim/{autoload,bundle}
 curl -LSso $HOME/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 git clone https://github.com/itchyny/lightline.vim $HOME/.vim/bundle/lightline.vim
-git clone https://github.com/scrooloose/nerdtree.git $HOME/.vim/bundle/nerdtree
 mkdir $HOME/.vim/{bkup,tmp}
 cp $HOME/projects/resources/dotfiles/vimrc $HOME/.vimrc
 echo "done!"
