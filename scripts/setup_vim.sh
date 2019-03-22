50e1b202718fb3df3f803c08bd839527a132a309
Josh Lawrence <josh.lawrence@dmpinvestments.net>
2019-03-22 16:05:08 -0500

    finalizing the vim setup stuff

--- scripts/setup_vim.sh
+++ scripts/setup_vim.sh
@@ -6,14 +6,15 @@ if [ -d "$HOME/.vim" ]; then
 fi
 
 # pathogen
-mkdir -p $HOME/.vim/{autoload,bundle}
+mkdir -p $HOME/.vim/{autoload,bundle,mysnippets}
 curl -LSso $HOME/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
 # plugins
 git clone https://github.com/itchyny/lightline.vim ~/.vim/bundle/lightline.vim
 git clone https://tpope.io/vim/sensible.git $HOME/.vim/bundle/sensible
-git clone git://github.com/SirVer/ultisnips.git $HOME/.vim/bundle/ultisnips
-git clone git://github.com/honza/vim-snippets.git $HOME/.vim/bundle/vim-snippets
+git clone https://github.com/SirVer/ultisnips.git $HOME/.vim/bundle/ultisnips
+
+touch "$HOME/.vim/mysnippets/all.snippets"
 
 vim -u NONE -c "helptags sensible/doc" -c q
 vim -u NONE -c "helptags ultisnips/doc" -c q
