b6eba543cac05b1e171ed20c61bbda29d3b48fac
Josh Lawrence <josh.lawrence@dmpinvestments.net>
2019-03-26 13:31:55 -0500

    removed ultisnips from vim setup

--- scripts/setup_vim.sh
+++ scripts/setup_vim.sh
@@ -12,11 +12,7 @@ curl -LSso $HOME/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 # plugins
 git clone https://github.com/itchyny/lightline.vim ~/.vim/bundle/lightline.vim
 git clone https://tpope.io/vim/sensible.git $HOME/.vim/bundle/sensible
-git clone https://github.com/SirVer/ultisnips.git $HOME/.vim/bundle/ultisnips
-
-touch "$HOME/.vim/mysnippets/all.snippets"
 
 vim -u NONE -c "helptags sensible/doc" -c q
-vim -u NONE -c "helptags ultisnips/doc" -c q
 
 echo "done! don't forget to copy your vimrc."
