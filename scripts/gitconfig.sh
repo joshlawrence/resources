6052b3ea01daf19bf867ef851e6bfdb7d70df368
Josh Lawrence <josh.lawrence@dmpinvestments.net>
2019-05-22 15:30:59 -0500

    more work on the bash_profile

---
+++ scripts/gitconfig.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+git config --global user.name "Josh Lawrence"
+git config --global user.email "josh.lawrence@dmpinvestments.net"
+git config --global credential.helper osxkeychain
+git config --global core.editor "code --wait"
+# git config --global core.editor vim
+git config --global color.ui auto
