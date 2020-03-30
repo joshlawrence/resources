0c7703920f0f8a938e4a368987a57a9c7e2169fb
Josh Lawrence <josh.lawrence@dmpinvestments.net>
2020-03-30 15:35:57 -0500

    updated init.el

---
+++ dotfiles/init2.el
@@ -0,0 +1,271 @@
+;;; -*- lexical-binding: t -*-
+
+;; package registry
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(package-initialize)
+
+;; bootstrap use-package
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(require 'use-package)
+
+;; pass shell environment from osx to emacs
+(use-package exec-path-from-shell
+  :ensure t)
+(when (memq window-system '(mac ns))
+  (exec-path-from-shell-initialize))
+
+(setq tramp-default-method "ssh")
+
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file 'noerror)
+
+(use-package evil
+  :config
+  (evil-mode 1))
+
+;; Both command keys are 'Super'
+(setq mac-right-command-modifier 'super)
+(setq mac-command-modifier 'super)
+
+;; Option or Alt is naturally 'Meta'
+(setq mac-option-modifier 'meta)
+
+;; Right Alt (option) can be used to enter symbols like em dashes '—' and euros '€' and stuff.
+(setq mac-right-option-modifier 'nil)
+
+;; Control is control, and you also need to change Caps Lock to Control in the Keyboard
+;; preferences in macOS.
+
+;; Show keybindings cheatsheet
+(use-package which-key
+  :config
+  (which-key-mode)
+  (setq which-key-idle-delay 0.5))
+
+;; defaults
+
+;; Smoother and nicer scrolling
+(setq scroll-margin 10
+   scroll-step 1
+   next-line-add-newlines nil
+   scroll-conservatively 10000
+   scroll-preserve-screen-position 1)
+
+(setq mouse-wheel-follow-mouse 't)
+(setq mouse-wheel-scroll-amount '(1 ((shift) . 1)))
+
+;; Don't bother with auto save and backups.
+(setq auto-save-default nil)
+(setq make-backup-files nil)
+
+;; Warn only when opening files bigger than 100MB
+(setq large-file-warning-threshold 100000000)
+
+;; Move file to trash instead of removing.
+(setq-default delete-by-moving-to-trash t)
+
+;; Revert (update) buffers automatically when underlying files are changed externally.
+(global-auto-revert-mode t)
+
+(setq
+ inhibit-startup-message t         ; Don't show the startup message...
+ inhibit-startup-screen t          ; ... or screen
+ cursor-in-non-selected-windows t  ; Hide the cursor in inactive windows
+
+ echo-keystrokes 0.1               ; Show keystrokes right away, don't show the message in the scratch buffer
+ initial-scratch-message nil       ; Empty scratch buffer
+ initial-major-mode 'org-mode      ; Org mode by default
+ sentence-end-double-space nil     ; Sentences should end in one space, come on!
+ confirm-kill-emacs 'y-or-n-p      ; y and n instead of yes and no when quitting
+ help-window-select t              ; Select help window so it's easy to quit it with 'q'
+)
+
+(fset 'yes-or-no-p 'y-or-n-p)      ; y and n instead of yes and no everywhere else
+(delete-selection-mode 1)          ; Delete selected text when typing
+(global-unset-key (kbd "s-p"))     ; Don't print
+
+;; We need Emacs kill ring and system clipboard to be independent. Simpleclip is the solution to that.
+(use-package simpleclip
+  :config
+  (simpleclip-mode 1))
+
+(global-set-key (kbd "s-s") 'save-buffer)             ;; save
+(global-set-key (kbd "s-S") 'write-file)              ;; save as
+(global-set-key (kbd "s-q") 'save-buffers-kill-emacs) ;; quit
+(global-set-key (kbd "s-a") 'mark-whole-buffer)       ;; select all
+;; (global-set-key (kbd "s-z") 'undo)
+
+;; delete trailing spaces and add new line in the end of a file on save.
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
+(setq require-final-newline t)
+
+;; Linear undo and redo.
+(use-package undo-tree
+  :diminish undo-tree-mode
+  :init
+  (progn
+    (global-undo-tree-mode)
+    (setq undo-tree-history-directory-alist '(("." . "~/.emacs.d/tmp/undo"))
+          undo-tree-auto-save-history t
+          undo-tree-visualizer-timestamps t
+          undo-tree-visualizer-diff t)))
+
+(global-set-key (kbd "s-z") 'undo-tree-undo)
+(global-set-key (kbd "s-Z") 'undo-tree-redo)
+
+;; visuals and theme
+
+(add-to-list 'default-frame-alist '(fullscreen . maximized))
+
+(use-package doom-themes
+  :config
+  ;; Global settings (defaults)
+  (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
+        doom-themes-enable-italic t) ; if nil, italics is universally disabled
+  (load-theme 'doom-one t)
+
+  ;; Enable flashing mode-line on errors
+  (doom-themes-visual-bell-config)
+  
+  ;; Enable custom neotree theme (all-the-icons must be installed!)
+  (doom-themes-neotree-config)
+
+    ;; Corrects (and improves) org-mode's native fontification.
+  (doom-themes-org-config))
+
+(set-default-font "SF Mono 16")
+;(set-default-font "IBM Plex Mono 16")
+
+;; M-x all-the-icons-install-fonts
+(use-package all-the-icons)
+
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+
+(global-visual-line-mode 1)
+
+(global-hl-line-mode 1)
+
+;; hide minor modes from modeline
+(use-package rich-minority
+  :config
+  (rich-minority-mode 1)
+  (setf rm-blacklist ""))
+
+(use-package neotree
+  :config
+  (setq neo-window-width 32
+        neo-create-file-auto-open t
+        neo-banner-message nil
+        neo-show-updir-line t
+        neo-window-fixed-size nil
+        neo-vc-integration nil
+        neo-mode-line-type 'neotree
+        neo-smart-open t
+        neo-show-hidden-files t
+        neo-mode-line-type 'none
+        neo-auto-indent-point t)
+  (setq neo-theme (if (display-graphic-p) 'nerd 'arrow))
+  (setq neo-hidden-regexp-list '("venv" "\\.pyc$" "~$" "\\.git" "__pycache__" ".DS_Store"))
+  (global-set-key [f8] 'neotree-toggle))
+
+;; Show vi-like tilde in the fringe on empty lines.
+(use-package vi-tilde-fringe
+  :config
+  (global-vi-tilde-fringe-mode 1))
+
+;; Show full path in the title bar.
+(setq-default frame-title-format "%b (%f)")
+
+;; tabs to spaces
+(setq-default indent-tabs-mode nil)
+
+;; Disable blinking cursor.
+(blink-cursor-mode 0)
+
+;; text editing
+
+(use-package smartparens
+  :diminish
+  :config
+  (require 'smartparens-config)
+  (smartparens-global-mode t)
+  (show-smartparens-global-mode t))
+
+;; upcase and lowercase word or region, if selected.
+;; to capitalize or un-capitalize word use alt+c and alt+l
+(global-set-key (kbd "M-u") 'upcase-dwim)   ;; Alt+u upcase
+(global-set-key (kbd "M-l") 'downcase-dwim) ;; Alt-l lowercase
+
+;; comment line or region.
+(global-set-key (kbd "s-/") 'comment-line)
+
+;; window management
+
+;; when split is automatic, always split windows vertically
+(setq split-height-threshold 0)
+(setq split-width-threshold nil)
+
+;; go to other windows easily with one keystroke cmd-something.
+(global-set-key (kbd "s-1") (kbd "C-x 1"))  ;; Cmd-1 kill other windows (keep 1)
+(global-set-key (kbd "s-2") (kbd "C-x 2"))  ;; Cmd-2 split horizontally
+(global-set-key (kbd "s-3") (kbd "C-x 3"))  ;; Cmd-3 split vertically
+(global-set-key (kbd "s-0") (kbd "C-x 0"))  ;; Cmd-0...
+(global-set-key (kbd "s-w") (kbd "C-x 0"))  ;; ...and Cmd-w to close current window
+
+;; Move between windows with Control-Command-Arrow and with =Cmd= just like in iTerm.
+(use-package windmove
+  :config
+  (global-set-key (kbd "<C-s-left>")  'windmove-left)  ;; Ctrl+Cmd+left go to left window
+  (global-set-key (kbd "s-[")  'windmove-left)         ;; Cmd+[ go to left window
+
+  (global-set-key (kbd "<C-s-right>") 'windmove-right) ;; Ctrl+Cmd+right go to right window
+  (global-set-key (kbd "s-]")  'windmove-right)        ;; Cmd+] go to right window
+
+  (global-set-key (kbd "<C-s-up>")    'windmove-up)    ;; Ctrl+Cmd+up go to upper window
+  (global-set-key (kbd "s-{")  'windmove-up)           ;; Cmd+Shift+[ go to upper window
+
+  (global-set-key (kbd "<C-s-down>")  'windmove-down)  ;; Ctrl+Cmd+down go to down window
+  (global-set-key (kbd "s-}")  'windmove-down))        ;; Cmd+Shift+] got to down window
+
+;; magit
+
+(use-package magit
+  :config
+  (global-set-key (kbd "s-g") 'magit-status))   ;; Cmd+g for git status
+
+(use-package git-gutter
+  :diminish
+  :config
+  (global-git-gutter-mode 't)
+  (set-face-background 'git-gutter:modified 'nil)   ;; background color
+  (set-face-foreground 'git-gutter:added "green4")
+  (set-face-foreground 'git-gutter:deleted "red"))
+
+;; language packages
+
+(use-package yaml-mode)
+(use-package haml-mode)
+(use-package markdown-mode)
+
+;; org mode
+
+;; Some basic Org defaults
+(use-package org
+  :config
+  (setq org-startup-indented t)         ;; Visually indent sections. This looks better for smaller files.
+  (setq org-src-tab-acts-natively t)    ;; Tab in source blocks should act like in major mode
+  (setq org-src-preserve-indentation t)
+  (setq org-log-into-drawer t)          ;; State changes for todos and also notes should go into a Logbook drawer
+  (setq org-src-fontify-natively t)     ;; Code highlighting in code blocks
+  (setq org-log-done 'time)             ;; Add closed date when todo goes to DONE state
+  (setq org-support-shift-select t))    ;; Allow shift selection with arrows.
+
+(setq org-directory "~/org")
+
+(setq org-agenda-files '("~/org"))
+
+;; EOF
