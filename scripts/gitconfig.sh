a60cbfcf1386e5ef9122e59f3f474554799fb649
Josh Lawrence <josh.lawrence@dmpinvestments.net>
2021-02-26 08:53:39 -0600

    SUBJECT: updated gitconfig.sh with new options
    gitconfig.sh now asks for an email address and uses os-specific
    options for setting the credential helper.

--- scripts/gitconfig.sh
+++ scripts/gitconfig.sh
@@ -1,8 +1,43 @@
 #!/usr/bin/env bash
+# gitconfig.sh
+
+# prompt the user for an email address
+printf "%s\n" "Enter your email address and press [ENTER] "
+read -rp "email: " my_email
+
+# remove existing gitconfig
+if [ -f "$HOME/.gitconfig" ]; then
+    rm "$HOME/.gitconfig"
+else
+    echo "no ~/.gitconfig file found, proceeding..."
+fi
 
 git config --global user.name "Josh Lawrence"
-git config --global user.email "josh.lawrence@dmpinvestments.net"
-git config --global credential.helper osxkeychain
-git config --global core.editor "code --wait"
-# git config --global core.editor vim
-git config --global color.ui auto
+git config --global user.email "$my_email"
+
+# os-specific
+if [[ $OSTYPE == darwin* ]]; then
+    git config --global credential.helper osxkeychain
+elif [[ $OSTYPE == linux-gnu* ]]; then
+    git config --global credential.helper cache
+fi
+
+git config --global core.editor "nano -w"
+git config --global core.autocrlf input
+git config --global alias.fuckit "reset --hard"
+git config --global push.default simple
+git config --global pull.rebase true
+#git config --global fetch.prune true
+git config --global init.defaultBranch master
+
+# create git commit.template
+cat << EOF > "$HOME/.gitmessage.txt"
+SUBJECT: explaining the commit in one line
+
+The body of the commit message can be several paragraphs, and
+please do proper word-wrap and keep columns shorter than about
+74 characters or so. That way "git log" will show things
+nicely even when it's indented.
+EOF
+
+git config --global commit.template "$HOME/.gitmessage.txt"
