b80e893925e612eda4169b7237df201c986b0583
Josh Lawrence <josh.lawrence@dmpinvestments.net>
2021-04-28 15:39:57 -0500

    update to setup-xorg for freebsd

--- freebsd/scripts/setup-xorg.sh
+++ freebsd/scripts/setup-xorg.sh
@@ -20,6 +20,19 @@ install_kde() {
     pkg install -y kde5
 }
 
+# install sddm
+install_sddm() {
+    pkg install -y sddm plasma5-sddm-kcm
+    sysrc sddm_enable="YES"
+    cat <<EOF >/usr/local/etc/polkit-1/rules-d/40-wheel-group.rules
+polkit.addRule(function(action, subject) {
+    if (subject.isInGroup("wheel")) {
+    	return polkit.Result.YES;
+    }
+});
+EOF
+}
+
 # main
 cat << EOF
 setup-xorg.sh
@@ -29,13 +42,14 @@ this script will do the following on a fresh freebsd 13 install:
     - install xorg and other useful packages
     - enable and start the dbus service
     - install kde5
+    - install the sddm display manager and enable the service
 
 NOTE! you must add the proc filesystem to /etc/fstab before you can continue!
 
 suggested entry:
     proc /proc procfs rw 0 0
 
-then issue `mount -a`.
+then issue mount -a.
 
 EOF
 
@@ -50,15 +64,18 @@ echo "installing xorg..."
 install_xorg
 echo "installing kde5..."
 install_kde
+echo "installing sddm..."
+install_sddm
 
 # complete setup
 cat << EOF
 
-setup is complete.
+setup is complete.  sddm will start on the next boot.
 
 to run kde from startx, add this to ~/.xinitrc:
 
     exec ck-launch-session startplasma-x11
 
 good luck!
+
 EOF
